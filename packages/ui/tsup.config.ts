4a1ae737566e5246fdbc82afa5cae698e8b30624
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2024-03-22 17:46:56 +0530

    chore: updated config

--- packages/ui/tsup.config.ts
+++ packages/ui/tsup.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'tsup'
 
+const isProduction = process.env.NODE_ENV === 'production'
 export default defineConfig({
     entryPoints: ['src/index.ts'], // Entry point of your library
     format: ['cjs', 'esm'], // Output formats for CommonJS and ESM modules
@@ -8,4 +9,5 @@ export default defineConfig({
     sourcemap: true, // Generate sourcemaps
     clean: true, // Clean the output directory before building
     outDir: 'lib', // Output directory
+    dts: true, // Generate .d.ts file
 })
