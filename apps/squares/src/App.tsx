6596cfe9d3d536485b36d7aff3d1131f31b01a5d
ArpitaGanatra <ganatraarpita@gmail.com>
2024-03-22 18:03:19 +0530

    feat: husky

--- apps/squares/src/App.tsx
+++ apps/squares/src/App.tsx
@@ -20,7 +20,7 @@ function App() {
             <div className="card">
                 <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
                 <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
+                    Edits <code>src/App.tsx</code> and save to test HMR
                 </p>
             </div>
             <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
