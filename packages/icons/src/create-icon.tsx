263da4c0ec6a55fa57d66184e2fbfdef23628c69
ArpitaGanatra <ganatraarpita@gmail.com>
2024-03-25 17:21:17 +0530

    feat: create icon comp

---
+++ packages/icons/src/create-icon.tsx
@@ -0,0 +1,35 @@
+import React, { forwardRef } from 'react'
+
+interface IconOptions extends React.SVGProps<SVGSVGElement> {
+    name: string
+    displayName: string
+    paths: { d: string; opacity?: number; fill: string; stroke: string }[]
+    gpaths?: { d: string; opacity?: number; fill: string; stroke: string }[]
+    viewBox: string
+}
+
+export function CreateIcon(options: IconOptions) {
+    const { viewBox, paths, name, displayName } = options
+
+    const renderedPaths = paths.map((value, index) => (
+        <path
+            key={index}
+            // fillRule="evenodd"
+            // clipRule="evenodd"
+            d={value.d}
+            fill={value.fill || 'none'} // default to 'none' if fill is not true
+            stroke={value.stroke || 'none'} // default to 'none' if stroke is not true
+            opacity={value.opacity || 1}
+        />
+    ))
+
+    const Comp = forwardRef<SVGSVGElement, React.SVGProps<SVGSVGElement>>((props, ref) => (
+        <svg ref={ref} width="24" height="24" viewBox={viewBox} fill="none" {...props}>
+            {renderedPaths}
+        </svg>
+    ))
+
+    Comp.displayName = displayName
+
+    return Comp
+}
