c7c093890c35cf6defdbcfdb9ed485471d97bbec
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2024-04-03 11:21:28 +0530

    fix: component styles

--- packages/ui/tailwind.config.ts
+++ packages/ui/tailwind.config.ts
@@ -2,7 +2,7 @@ import { theme } from './src/config/theme'
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+    content: ['./src/**/*.{js,jsx,ts,tsx}, ./src/components/**/*.{js,jsx,ts,tsx}'],
     darkMode: 'class',
     theme: theme,
 }
