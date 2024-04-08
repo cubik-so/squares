fa3d514c7767acfa2621d97749428ca87eac97e6
ArpitaGanatra <ganatraarpita@gmail.com>
2024-04-08 18:00:44 +0530

    feat: checkbox component

--- apps/test/src/app/page.tsx
+++ apps/test/src/app/page.tsx
@@ -1,12 +1,10 @@
 'use client'
-import { Alert, Button, Modal, ModalHeader } from '@squares/ui'
-import { useState } from 'react'
+import { Button, Checkbox } from '@squares/ui'
 
 export default function Home() {
-    const [open, setOpen] = useState(false)
     return (
         <main className=" flex min-h-screen flex-col items-center justify-center gap-8 p-24">
-            <Button size="lg" variant="outline" leftIconName="plus" onClick={() => setOpen(true)}>
+            <Button size="lg" variant="outline" leftIconName="plus">
                 asdfsa
             </Button>
             <Button
@@ -19,58 +17,11 @@ export default function Home() {
             >
                 asdfsa
             </Button>
-
-            {open && (
-                <Modal open={open} className="" onClose={() => setOpen(false)} dialogSize="lg">
-                    <ModalHeader heading="jkhjhjhk" closeIcon={false} />
-                    <p>askjdhfjkahsdf</p>
-                    <p>askjdhfjkahsdf</p>
-                    <p>askjdhfjkahsdf</p>
-                    <p>askjdhfjkahsdf</p>
-                    <p>askjdhfjkahsdf</p>
-                </Modal>
-            )}
-            <Alert
-                description="This is a description"
-                iconName="alert-info-circle"
-                state={'info'}
-                variant="default"
+            <Checkbox
+                onCheckedChange={(e) => {
+                    console.log(e)
+                }}
             />
-            <Alert
-                description="This is a description"
-                iconName="alert-info-circle"
-                state={'info'}
-                variant="textOnly"
-            />
-            <Alert
-                description="This is a description"
-                iconName="alert-info-circle"
-                state={'default'}
-                variant="leftOutline"
-            />
-            <Alert
-                description="This is a description"
-                iconName="alert-info-circle"
-                state={'innovative'}
-                variant="border"
-            />
-
-            <Alert title="hello world" iconName="alert-info-circle" state={'success'} />
-            <Alert
-                title="hello world"
-                description="This is a description"
-                iconName="alert-info-circle"
-                state={'error'}
-            >
-                <Button
-                    variant={'danger'}
-                    className="!h-fit !bg-transparent !p-0"
-                    rightIconName="arrow-right"
-                    size="sm"
-                >
-                    Check this
-                </Button>
-            </Alert>
         </main>
     )
 }
---
+++ packages/ui/src/components/checkbox/checkbox.tsx
@@ -0,0 +1,56 @@
+'use client'
+
+import * as CheckboxPrimitive from '@radix-ui/react-checkbox'
+import { cva } from 'class-variance-authority'
+import React from 'react'
+import { cn } from '@utils/cn'
+import Icon from '@/icons'
+import type { VariantProps } from 'class-variance-authority'
+
+const checkboxVariants = cva(
+    'peer border-2 border-color-border-primary-subdued shrink-0 rounded-[6px] focus-visible:outline-none focus-visible:ring-1 hover:border-form-checkbox-border-hovered focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 hover:bg-form-checkbox-surface-hovered disabled:border-form-checkbox-border-disabled disabled:bg-form-checkbox-surface-disabled data-[state=checked]:bg-form-checkbox-surface-default-checked data-[state=checked]:border-form-checkbox-surface-default-checked',
+    {
+        variants: {
+            size: {
+                sm: 'h-5 w-5',
+                md: 'h-6 w-6',
+            },
+        },
+        defaultVariants: {
+            size: 'md',
+        },
+    },
+)
+
+interface CheckboxProps extends VariantProps<typeof checkboxVariants> {
+    className?: string
+    onCheckedChange: (e: any) => void
+    defaultChecked?: boolean
+}
+
+export const Checkbox = React.forwardRef<
+    React.ElementRef<typeof CheckboxPrimitive.Root>,
+    CheckboxProps
+>(({ className, size = 'md', defaultChecked = false, onCheckedChange, ...props }, ref) => (
+    <CheckboxPrimitive.Root
+        ref={ref}
+        className={cn(className, checkboxVariants({ size }))}
+        defaultChecked={defaultChecked}
+        onChange={onCheckedChange}
+        {...props}
+    >
+        <CheckboxPrimitive.Indicator
+            className={cn('flex items-center justify-center text-current')}
+        >
+            <Icon
+                name="check"
+                strokeWidth={3}
+                width={16}
+                height={16}
+                color="color-fg-primary-depth"
+            />
+        </CheckboxPrimitive.Indicator>
+    </CheckboxPrimitive.Root>
+))
+
+Checkbox.displayName = CheckboxPrimitive.Root.displayName
