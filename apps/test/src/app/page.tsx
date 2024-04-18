366f02d7e911ad8c9d5a51bb36109d36f8bb3567
ArpitaGanatra <ganatraarpita@gmail.com>
2024-04-18 13:23:21 +0530

    feat: tabs & vertical tabs component

--- apps/test/src/app/page.tsx
+++ apps/test/src/app/page.tsx
@@ -1,36 +1,81 @@
 'use client'
 
-import { CubikTable } from '@squares/ui'
-
-const columns = [
-    {
-        accessorKey: 'id', // accessor is the "key" in the data
-        header: () => 'ID',
-        cell: (info: any) => info.getValue(),
-    },
-    {
-        accessorKey: 'name',
-        header: () => 'Name',
-        cell: (info: any) => info.getValue(),
-    },
-    {
-        accessorKey: 'age',
-        header: () => 'Age',
-        cell: (info: any) => info.getValue(),
-    },
-]
-
-// Create some mock data
-const data = [
-    { id: 1, name: 'Alice', age: 25 },
-    { id: 2, name: 'Bob', age: 30 },
-    { id: 3, name: 'Carol', age: 28 },
-]
+import {
+    Tab,
+    TabList,
+    TabPanel,
+    TabPanels,
+    Tabs,
+    Text,
+    VerticalTab,
+    VerticalTabList,
+    VerticalTabPanel,
+    VerticalTabPanels,
+    VerticalTabs,
+} from '@squares/ui'
 
 export default function Home() {
     return (
         <main className=" flex min-h-screen flex-col items-center justify-center gap-8 p-24">
-            <CubikTable columns={columns} data={data} />
+            <Tabs defaultValue={2} size="sm" className="">
+                <TabList>
+                    <Tab value={0}>
+                        <div className="p-2">TabItem1</div>
+                    </Tab>
+                    <Tab value={1}>
+                        {' '}
+                        <div className="p-2">TabItem2</div>
+                    </Tab>
+                    <Tab value={2}>
+                        {' '}
+                        <div className="p-2">Tabitem3</div>
+                    </Tab>
+                </TabList>
+                <TabPanels>
+                    <TabPanel value={0}>
+                        <p>one!</p>
+                    </TabPanel>
+                    <TabPanel value={1}>
+                        <p>two!</p>
+                    </TabPanel>
+                    <TabPanel value={2}>
+                        <p>three!</p>
+                    </TabPanel>
+                </TabPanels>
+            </Tabs>
+
+            <VerticalTabs defaultValue={'one'} size="sm" className="">
+                <VerticalTabList>
+                    <VerticalTab value={'one'} leftIconName="search">
+                        TabItem1
+                    </VerticalTab>
+                    <VerticalTab value={'two'} leftIconName="search">
+                        {' '}
+                        TabItem2
+                    </VerticalTab>
+                    <VerticalTab value={'three'} leftIconName="search">
+                        {' '}
+                        Tabitem3
+                    </VerticalTab>
+                </VerticalTabList>
+                <VerticalTabPanels className="">
+                    <VerticalTabPanel value={'one'}>
+                        <Text className="l2" color={'primary'}>
+                            one!
+                        </Text>
+                    </VerticalTabPanel>
+                    <VerticalTabPanel value={'two'}>
+                        <Text className="l2" color={'primary'}>
+                            two!
+                        </Text>
+                    </VerticalTabPanel>
+                    <VerticalTabPanel value={'three'}>
+                        <Text className="l2" color={'primary'}>
+                            three!
+                        </Text>
+                    </VerticalTabPanel>
+                </VerticalTabPanels>
+            </VerticalTabs>
         </main>
     )
 }
---
+++ packages/ui/src/components/tabs/vertical-tabs.tsx
@@ -0,0 +1,90 @@
+'use client'
+
+import React from 'react'
+import * as RadixTabs from '@radix-ui/react-tabs'
+import { cn } from '@utils/cn'
+import Icon from '@/icons'
+import type { ReactNode } from 'react'
+
+interface TabsProps {
+    children: ReactNode
+    defaultValue: string
+    size: 'xxs' | 'xs' | 'sm' | 'md' | 'lg'
+    className?: string
+}
+
+type TabListProps = {
+    children: ReactNode
+    className?: string
+}
+
+type TabProps = {
+    children: ReactNode
+    value: string
+    className?: string
+    leftIconName?: string
+}
+
+type TabPanelsProps = {
+    children: ReactNode
+    className?: string
+}
+
+type TabPanelProps = {
+    children: ReactNode
+    value: string
+    className?: string
+}
+
+const VerticalTabs = ({ children, defaultValue, className }: TabsProps) => (
+    <RadixTabs.Root
+        defaultValue={defaultValue}
+        orientation="vertical"
+        className={cn('flex h-full w-full pointer-events-auto', className)}
+    >
+        {children}
+    </RadixTabs.Root>
+)
+
+const VerticalTabList: React.FC<TabListProps> = ({ children, className }) => (
+    <RadixTabs.List className={cn('shrink-0 flex flex-col gap-2 px-6 w-[265px]', className)}>
+        {children}
+    </RadixTabs.List>
+)
+
+const VerticalTab = ({ children, value, className, leftIconName }: TabProps) => {
+    return (
+        <RadixTabs.Trigger
+            value={value}
+            className={cn(
+                'flex items-center justify-start leading-none select-none rounded-lg p-2 bg-menu-list-item-surface-default text-menu-list-item-fg-default data-[state=active]:bg-menu-list-item-surface-hovered data-[state=active]:text-menu-list-item-fg-hovered stroke-menu-list-item-icon data-[state=active]:stroke-menu-list-item-fg-hovered data-[state=active]:shadow-current data-[state=active]:focus:relative outline-none cursor-pointer',
+                className,
+            )}
+        >
+            <div className="flex gap-[10px] items-center ">
+                {leftIconName && <Icon name={leftIconName} height={20} width={20} />}
+                {children}
+            </div>
+        </RadixTabs.Trigger>
+    )
+}
+
+const VerticalTabPanels = ({ children, className }: TabPanelsProps) => (
+    <div className={cn('w-full h-full', className)}>{children}</div>
+)
+
+const VerticalTabPanel = ({ children, value, className }: TabPanelProps) => (
+    <RadixTabs.Content
+        value={value}
+        className={cn(
+            'grow p-3 2xl:p-5 bg-modal-body-surface outline-none w-full h-full',
+            className,
+        )}
+    >
+        {children}
+    </RadixTabs.Content>
+)
+
+VerticalTab.displayName = 'VerticalTab'
+
+export { VerticalTabs, VerticalTab, VerticalTabList, VerticalTabPanels, VerticalTabPanel }
