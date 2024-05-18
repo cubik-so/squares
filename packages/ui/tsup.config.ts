3ff5649d159d959d771303163fe5e13c60e90162
ArpitaGanatra <ganatraarpita@gmail.com>
2024-05-18 11:29:38 +0530

    fix: css

--- packages/ui/tsup.config.ts
+++ packages/ui/tsup.config.ts
@@ -4,7 +4,7 @@ const isProduction = process.env.NODE_ENV === 'production'
 export default defineConfig({
     entryPoints: ['src/**/*'], // Entry point of your library
     format: ['cjs', 'esm'], // Output formats for CommonJS and ESM modules
-    minify: false, // Whether to minify output
+    minify: true, // Whether to minify output
     splitting: true, // Enable code splitting for better tree shaking
     sourcemap: false, // Generate sourcemaps
     clean: true, // Clean the output directory before building
