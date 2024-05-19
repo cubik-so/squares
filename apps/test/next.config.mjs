56522e0f1a72c1101f2e08c2e2e7c60fc1ab2f93
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2024-05-19 21:44:21 +0530

    fix: build

--- apps/test/next.config.mjs
+++ apps/test/next.config.mjs
@@ -1,9 +1,10 @@
+import withBundleAnalyzer from '@next/bundle-analyzer'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
     transpilePackages: ['@squaress/ui'],
-    cssModules: true,
 }
 
 export default nextConfig
--- packages/ui/tsup.config.ts
+++ packages/ui/tsup.config.ts
@@ -1,14 +1,15 @@
 import { defineConfig } from 'tsup'
 
-const isProduction = process.env.NODE_ENV === 'production'
 export default defineConfig({
-    entryPoints: ['src/**/*'], // Entry point of your library
     format: ['cjs', 'esm'], // Output formats for CommonJS and ESM modules
-    minify: true, // Whether to minify output
-    splitting: true, // Enable code splitting for better tree shaking
-    sourcemap: false, // Generate sourcemaps
+    minify: false, // Whether to minify output
     clean: true, // Clean the output directory before building
     outDir: 'lib', // Output directory
     dts: true, // Generate .d.ts file
     external: ['react', 'react-dom'],
+    entry: ['src/index.ts', 'src/config', 'src/styles'],
+    treeshake: true,
+    sourcemap: 'inline',
+    splitting: false,
+    injectStyle: false,
 })
