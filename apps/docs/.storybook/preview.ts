d8a9a1e0f5d635304a0b2e335bea1d1f3cc8aa18
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2024-05-20 10:13:59 +0530

    fix: darkmode

--- apps/docs/.storybook/preview.ts
+++ apps/docs/.storybook/preview.ts
@@ -1,6 +1,12 @@
 import type { Preview } from '@storybook/react'
 
 const preview: Preview = {
+    globalTypes: {
+        darkMode: {
+            defaultValue: true, // Enable dark mode by default on all stories
+        },
+    },
+
     parameters: {
         controls: {
             matchers: {
--- apps/docs/tailwind.config.ts
+++ apps/docs/tailwind.config.ts
@@ -2,6 +2,7 @@ import { theme } from '@squaress/ui/theme'
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
+    darkMode: 'class',
     content: [
         './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
         './src/components/**/*.{js,ts,jsx,tsx,mdx}',
