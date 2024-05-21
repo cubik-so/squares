10960a5c27a8771f7cfcbcdcd3b1134c2e5829a7
ArpitaGanatra <ganatraarpita@gmail.com>
2024-05-21 17:27:00 +0530

    fix: drawer scroll

--- packages/ui/src/components/drawer/drawer.tsx
+++ packages/ui/src/components/drawer/drawer.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-
 import { Drawer } from 'vaul'
 import { cn } from '@utils/cn'
 import type { ClassValue } from 'clsx'
@@ -38,35 +37,18 @@ interface DrawerContentProps {
     className?: ClassValue
 }
 
-// export const VaulDrawerContent = ({ children, className }: DrawerContentProps) => {
-//     return (
-//         <Drawer.Content
-//             className={cn(
-//                 // 'fixed h-fit shadow-2xl dark:shadow-black pt-2 focus:outline-none overflow-scroll bg-color-surface-primary',
-//                 'fixed  bg-color-surface-primary shadow-2xl dark:shadow-black pt-2 focus:outline-none',
-//                 'bottom-0 left-0 right-0 w-full max-w-[100%] rounded-t-[12px]  max-h-[93%] ',
-//                 className,
-//             )}
-//         >
-//             <div className="mx-auto w-12 h-[3px] flex-shrink-0 rounded-full bg-color-surface-cool-transparent mb-2" />
-
-//             {children}
-//         </Drawer.Content>
-//     )
-// }
-
 export const VaulDrawerContent = ({ children, className }: DrawerContentProps) => {
     return (
         <Drawer.Content
             className={cn(
-                'fixed bg-color-surface-primary shadow-2xl dark:shadow-black pt-2 focus:outline-none h-[100%]',
-                'bottom-0 left-0 right-0 w-full max-w-[100%] rounded-t-[12px] max-h-[93%]',
+                'fixed bg-color-surface-primary shadow-2xl dark:shadow-black pt-2 focus:outline-none',
+                'bottom-0 left-0 right-0 w-full max-w-[100%] rounded-t-[12px]',
                 className,
             )}
-            // style={{ overflowY: 'auto' }} // Adding this style to enable vertical scrolling
+            style={{ maxHeight: '93%', display: 'flex', flexDirection: 'column' }}
         >
             <div className="mx-auto w-12 h-[3px] flex-shrink-0 rounded-full bg-color-surface-cool-transparent mb-2" />
-            <div className="overflow-scroll h-full max-h-[100%]">{children}</div>
+            <div style={{ flexGrow: 1, overflowY: 'auto' }}>{children}</div>
         </Drawer.Content>
     )
 }
@@ -91,7 +73,6 @@ export interface VaulDrawerProps {
 }
 
 export const VaulDrawer = ({ children, open, onOpenChange, ...props }: VaulDrawerProps) => {
-    console.log('first', open, onOpenChange)
     return (
         <Drawer.Root open={open} onOpenChange={onOpenChange} {...props}>
             {children}
