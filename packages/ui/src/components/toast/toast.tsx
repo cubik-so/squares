7e5e28c876bc2ff7e8d8c2081b802adc6a2cbe25
ArpitaGanatra <ganatraarpita@gmail.com>
2024-05-23 11:40:01 +0530

    fix: changed toast

--- packages/ui/src/components/toast/toast.tsx
+++ packages/ui/src/components/toast/toast.tsx
@@ -7,27 +7,30 @@ const CubikToaster = () => {
     const isMobile = useMediaQuery('(max-width: 768px)')
     return (
         <Toaster
+            richColors
             position={isMobile ? 'bottom-center' : 'bottom-center'}
-            loadingIcon={<Spinner size="sm" />}
+            icons={{
+                loading: <Spinner size="sm" />,
+            }}
             className="flex items-center w-fit max-w-[90%] md:!max-w-[100%] md:w-full justify-center"
             toastOptions={{
                 unstyled: true,
                 classNames: {
-                    toast: 'bgtoast-primary-surface shadow-lg !w-fit md:w-auto texttoast-primary-text py-2 md:py-3 px-3 md:px-4 mx-auto rounded-xl transition-all flex flex-row gap-1 md:gap-2 items-center justify-center',
+                    toast: 'bg-[var(--toast-primary-surface)] shadow-lg !w-fit md:w-auto text-[var(--toast-primary-text)] py-2 md:py-3 px-3 md:px-4 mx-auto rounded-xl transition-all flex flex-row gap-1 md:gap-2 items-center justify-center',
                     title: isMobile ? '!text-xs font-normal' : 'text-md font-normal',
                     description: isMobile
                         ? '!text-xs w-fit !font-light pr-3'
                         : 'text-sm w-fit !font-light pr-3',
                     actionButton:
-                        'underline underline-offset-4 !bg-transparent !border-none !textcolor-fg-primary-base',
+                        'underline underline-offset-4 !bg-transparent !border-none !text-[var(--color-fg-primary-base)]',
                     cancelButton: 'bg-orange-400',
                     closeButton: 'bg-lime-400',
-                    loader: '!bgtoast-loading-surface !dark:bordertoast-primary-transparent !texttoast-loading-text',
-                    error: '!bgtoast-error-surface !texttoast-error-text',
-                    success: 'bgtoast-success-surface texttoast-success-text',
+                    loader: '!bg-[var(--toast-loading-surface)] !dark:border-[var(--toast-primary-transparent)] !text-[var(--toast-loading-text)]',
+                    error: '!bg-[var(--toast-error-surface)] !text-[var(--toast-error-text)]',
+                    success: 'bg-[var(--toast-success-surface)] text-[var(--toast-success-text)]',
                     warning:
-                        'bgtoast-warning-surface texttoast-warning-text border bordercolor-border-primary-transparent',
-                    info: '!bgtoast-info-surface texttoast-info-text',
+                        'bg-[var(--toast-warning-surface)] text-[var(--toast-warning-text)] border border-[var(--color-border-primary-transparent)]',
+                    info: '!bg-[var(--toast-info-surface)] text-[var(--toast-info-text)]',
                 },
             }}
         />
