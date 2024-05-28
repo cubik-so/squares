939ed8649e70c0dd84408c122e7ff903039e5956
ArpitaGanatra <ganatraarpita@gmail.com>
2024-05-28 14:56:01 +0530

    fix: change

--- packages/ui/src/components/input-field/input-field.tsx
+++ packages/ui/src/components/input-field/input-field.tsx
@@ -1,10 +1,7 @@
- 
- 
 'use client'
 
 import React, { createContext, useContext, useState } from 'react'
-
-import { cn } from '../../../lib/utils'
+import { cn } from '@utils/cn'
 
 export const InputFieldContainerVariants = {
     variants: {
--- packages/ui/src/components/spinner/spinner.tsx
+++ packages/ui/src/components/spinner/spinner.tsx
@@ -1,3 +1,4 @@
+'use client'
 import { motion } from 'framer-motion'
 import { cn } from '@/utils/cn'
 import { useTheme } from '@/hooks/useTheme'
