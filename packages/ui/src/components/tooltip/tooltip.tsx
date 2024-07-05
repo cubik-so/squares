e59a77c12f1c8dccdf0170979fdbbd81b4bb1d45
ArpitaGanatra <ganatraarpita@gmail.com>
2024-07-05 17:44:56 +0530

    fix: tooltip

--- packages/ui/src/components/tooltip/tooltip.tsx
+++ packages/ui/src/components/tooltip/tooltip.tsx
@@ -21,11 +21,13 @@ export const Tooltip = ({ children, content, align = 'center', duration }: Toolt
                     <RadixTooltip.Content
                         align={align}
                         className={cn(
-                            'data-[state=delayed-open]:data-[side=top]:animate-slideDownAndFade data-[state=delayed-open]:data-[side=right]:animate-slideLeftAndFade data-[state=delayed-open]:data-[side=left]:animate-slideRightAndFade data-[state=delayed-open]:data-[side=bottom]:animate-slideUpAndFade text-violet11 select-none rounded-lg px-3 py-2 bg-color-bg-info-base text-white leading-none shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] will-change-[transform,opacity] max-w-[200px] md:max-w-xs',
+                            'data-[state=delayed-open]:data-[side=top]:animate-slideDownAndFade data-[state=delayed-open]:data-[side=right]:animate-slideLeftAndFade data-[state=delayed-open]:data-[side=left]:animate-slideRightAndFade data-[state=delayed-open]:data-[side=bottom]:animate-slideUpAndFade select-none rounded-lg px-3 py-2 bg-color-bg-info-base text-white leading-none shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] will-change-[transform,opacity] max-w-[200px] md:max-w-xs',
                         )}
                         sideOffset={5}
                     >
-                        <Text className="l3-light">{content}</Text>
+                        <Text className="l3-light" color="inherit">
+                            {content}
+                        </Text>
 
                         <RadixTooltip.Arrow className="fill-color-bg-info-base" />
                     </RadixTooltip.Content>
